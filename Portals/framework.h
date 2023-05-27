0ea7b1c76d341acb4c2cca6c1eef53189997f250
Hamm <mhamm@fiea.local>
2023-05-27 12:32:04 -0400

    First commit

---
+++ Portals/framework.h
@@ -0,0 +1,23 @@
+// header.h : include file for standard system include files,
+// or project specific include files
+//
+
+#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files
+#include <windows.h>
+#if defined(min)
+#undef min
+#endif
+
+#if defined(max)
+#undef max
+#endif
+// C RunTime Header Files
+#include <wrl.h>
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
